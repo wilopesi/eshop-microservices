bab1190061bc1db2daeb47b3e056132ec2c62809
wilopesi <willianlivialopes@hotmail.com>
2025-07-29 07:16:21 -0300

    defining ordering domain model

---
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿namespace Ordering.API;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApiServices(this IServiceCollection services)
+    {
+
+        return services;
+    }
+
+    public static WebApplication UseApiServices(this WebApplication app)
+    {
+
+        return app;
+    }
+}
