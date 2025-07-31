07fb0703fc20d40397c95e3bfc840836150cbcba
wilopesi <willianlivialopes@hotmail.com>
2025-07-31 08:24:47 -0300

    configure database interceptor for ordering database

--- src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -12,7 +12,10 @@ public static class DependencyInjection
 
         // Add services to the container.
         services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseSqlServer(connectionString));
+        {
+            options.AddInterceptors(new AuditableEntityInterceptor());
+            options.UseSqlServer(connectionString);
+        });
 
         // services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
 
--- src/Services/Ordering/Ordering.Infrastructure/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Infrastructure/GlobalUsing.cs
@@ -3,4 +3,4 @@ global using Microsoft.EntityFrameworkCore;
 global using Ordering.Domain.Models;
 global using Ordering.Domain.ValueObjects;
 global using Ordering.Domain.Abstractions;
-
+global using Ordering.Infrastructure.Data.Interceptors;
