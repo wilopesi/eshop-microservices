9a4f4fce8a6216915dc6f5de71bb304eaf8b0fd4
wilopesi <willianlivialopes@hotmail.com>
2025-08-02 21:48:44 -0300

    develop delete and update commands

--- src/Services/Ordering/Ordering.Application/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Application/GlobalUsing.cs
@@ -1,1 +1,8 @@
 ﻿global using Ordering.Domain.Enums;
+global using BuildingBlocks.CQRS;
+global using Ordering.Application.Data;
+global using Ordering.Application.Dtos;
+global using Ordering.Domain.Models;
+global using Ordering.Domain.ValueObjects;
+global using FluentValidation; 
+global using Ordering.Application.Exceptions;
--- src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderCommand.cs
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderCommand.cs
@@ -1,8 +1,4 @@
-﻿using BuildingBlocks.CQRS;
-using FluentValidation;
-using Ordering.Application.Dtos;
-
-namespace Ordering.Application.Orders.Commands.CreateOrder;
+﻿namespace Ordering.Application.Orders.Commands.CreateOrder;
 
 public record CreateOrderCommand(OrderDto Order)
     : ICommand<CreateOrderResult>;
